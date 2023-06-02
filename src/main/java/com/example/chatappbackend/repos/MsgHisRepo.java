974fb66ae45e99f4e89627fcdbf302edeca9f88c
vahabvahabzada <vahabvahabzad@gmail.com>
2023-06-02 16:43:27 +0400

    Fixing relationship issues

--- src/main/java/com/example/chatappbackend/repos/MsgHisRepo.java
+++ src/main/java/com/example/chatappbackend/repos/MsgHisRepo.java
@@ -8,10 +8,7 @@ import org.springframework.data.jpa.repository.Query;
 import com.example.chatappbackend.entities.Message;
 
 public interface MsgHisRepo extends JpaRepository<Message,Long>{
-    //@Query("select * from messages where ")
-    //public List<Message> findByMesgCouple(String mesgCoupleName);
     
-    //@Query(value="select messages.mesg_id,mbody,mfrom,mto,mesg_box_id from messages inner join mesgboxcorrespondence on messages.mesg_id=mesgboxcorrespondence.mesg_id where mesg_box_id=?",nativeQuery=true)
     @Query(value = "select * from messages where mfrom=?1 and mto=?2",nativeQuery = true)
     public List<Message> getMessageHistory(String mfrom,String mto);
 }
